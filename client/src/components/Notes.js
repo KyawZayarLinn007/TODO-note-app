83560b56c2a78aa6169c4e1921f30938ba8d704b
KyawZayarLinn <kyawzayarlinnkb10@gmail.com>
2022-10-03 23:21:05 +0630

    add form dialog

--- client/src/components/Notes.js
+++ client/src/components/Notes.js
@@ -12,7 +12,14 @@ import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import EditIcon from "@mui/icons-material/Edit";
 import Stack from "@mui/material/Stack";
 import Tooltip from "@mui/material/Tooltip";
+import TextField from '@mui/material/TextField';
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from '@mui/material/Slide';
 
+// notes item
 const Item = () => {
   return (
     <Card>
@@ -38,9 +45,24 @@ const Item = () => {
   );
 };
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 export default function Notes() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
+      {/* new btn */}
       <Stack
         direction="row"
         justifyContent="center"
@@ -53,11 +75,42 @@ export default function Notes() {
           size="large"
           color="info"
           startIcon={<AddIcon />}
+          onClick={handleClickOpen}
         >
           NEW
         </Button>
       </Stack>
 
+      {/* dialog */}
+      <Dialog fullWidth={true} open={open} onClose={handleClose} TransitionComponent={Transition}>
+        <DialogTitle>Add new note</DialogTitle>
+        <DialogContent>
+          <TextField
+            margin="dense"
+            id="title"
+            label="Title"
+            type="text"
+            fullWidth
+            variant="outlined"
+          />
+          <TextField
+            margin="dense"
+            id="body"
+            label="Body"
+            type="text"
+            fullWidth
+            variant="outlined"
+            multiline
+            rows={4}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Add</Button>
+        </DialogActions>
+      </Dialog>
+
+      {/* notes */}
       <Box sx={{ flexGrow: 1, margin: 2 }}>
         <Grid container spacing={4}>
           <Grid item xs={12} md={4} lg={3}>
