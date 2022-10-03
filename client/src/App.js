7ca5ccc291063cf258c4ec9f9fcda494022686ca
KyawZayarLinn <kyawzayarlinnkb10@gmail.com>
2022-10-03 16:34:20 +0630

    navbar init

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+
 const App = () => {
   return ( 
-    <h1>Hello TODO-note-app!</h1>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Home />} />
+      </Routes>
+    </div>
   );
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,63 @@
-const Navbar = () => {
-    return ( 
-        <div></div>
-    );
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Link, NavLink } from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+
+export default function Navbar() {
+  let activeStyle = {
+    textDecoration: "underline",
+  };
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 0 }}
+          >
+            <AutoStoriesIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link
+                to="/"
+                className="router-link"
+            >
+                Notes
+            </Link>
+          </Typography>
+
+          {/* register btn */}
+          <NavLink
+            to="/register"
+            className="router-link"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            <Button color="inherit">Register</Button>
+          </NavLink>
+
+          {/* login btn */}
+          <NavLink
+            to="/login"
+            className="router-link"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            <Button color="inherit">Login</Button>
+          </NavLink>
+
+          {/* logout btn */}
+          <Link to="/logout" className="router-link">
+            <Button color="inherit">Logout</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
- 
-export default Navbar;
