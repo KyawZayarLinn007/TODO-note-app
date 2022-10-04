6eb430b568f41a36f3bc1f195033a6dfda21be53
KyawZayarLinn <kyawzayarlinnkb10@gmail.com>
2022-10-04 11:44:32 +0630

    auth routes init

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,7 @@
+module.exports.register_post = (req, res) => {
+    res.send("Register");
+}
+
+module.exports.login_post = (req, res) => {
+    res.send("Login");
+}
---
+++ server/src/routes/authRoute.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { register_post, login_post } = require("../controllers/authController");
+const router = express.Router();
+
+router.post("/register", register_post);
+router.post("/login", login_post);
+
+module.exports = router;
