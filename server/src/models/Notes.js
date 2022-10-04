8d6bbd1ef795c8b1baa43ea5f2176eca2329e9ef
KyawZayarLinn <kyawzayarlinnkb10@gmail.com>
2022-10-04 10:25:27 +0630

    express server and mongoose init

---
+++ server/src/models/Notes.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+const Notes = mongoose.model("note", noteSchema);
+module.exports = Notes;
