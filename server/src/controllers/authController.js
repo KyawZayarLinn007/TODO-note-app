6149875e76899c3835d284d2ad37967fc54de9db
KyawZayarLinn <kyawzayarlinnkb10@gmail.com>
2022-10-04 14:06:23 +0630

    register and login routes init

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,7 +1,53 @@
-module.exports.register_post = (req, res) => {
-    res.send("Register");
+require("dotenv").config();
+const jwt = require("json-web-token");
+const bcrypt = require("bcrypt");
+const Users = require("../models/Users");
+
+module.exports.register_post = async (req, res) => {
+    const { email, password } = req.body;
+
+    //bcrypt hashed
+    let salt = await bcrypt.genSalt(parseInt(process.env.BCRYPT_SALT_ROUNDS));
+    let hashedPassword = await bcrypt.hash(password, salt);
+
+    try {
+        let user = await Users.create({email, password: hashedPassword});
+        res.json({
+            error: null,
+            data: {
+                id: user._id,
+                email: user.email
+            }
+        })
+    } catch (error) {
+        res.json({
+            error: error.message,
+            data: null
+        })
+    }
 }
 
-module.exports.login_post = (req, res) => {
-    res.send("Login");
+module.exports.login_post = async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        let user = await Users.findOne({email});
+        let isPassword = await bcrypt.compare(password, user.password);
+        if(isPassword){
+            res.json({
+                error: null,
+                data: "login success"
+            })
+        }else{
+            res.json({
+                error: "Incorrect Password!",
+                data: null
+            })
+        }
+    } catch (error) {
+        res.json({
+            error: "Email not registered!",
+            data: null
+        })
+    }
 }
